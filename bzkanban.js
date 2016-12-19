leif81/bzkanban

commit e8eb7d480aabe46ffb85c7f2820d10e5018ace1b

Allow hardcoding product and milestone
Now it's possible to hardcode bzProduct and bzProductMilestone if you
always want to use certain values.
Previously we were blowing away the values.

bzkanban.js
@@ -37,8 +37,15 @@ function initBzkanban() {
 }
 
 function loadParams() {
-    bzProduct = getURLParameter('product');
-    bzProductMilestone = getURLParameter('milestone');
+    var product = getURLParameter('product');
+    if (product !== null) {
+        bzProduct = product;
+    }
+
+    var milestone = getURLParameter('milestone');
+    if (milestone !== null) {
+        bzProductMilestone = milestone;
+    }
 
     var assignee = getURLParameter('assignee');
     if (assignee !== null) {

commit 524bcf2bdd3be9d191d65547e18d9f46640d0cc7

Prevent board from loading until product and milestone are selected
By default those fields are now an empty string.

Caused by recent commit:
e8eb7d480aabe46ffb85c7f2820d10e5018ace1b

bzkanban.js
@@ -104,7 +104,7 @@ function initNav() {
 
     loadProductsList();
 
-    if (bzProduct !== null) {
+    if (bzProduct !== "") {
         loadMilestonesList();
     }
 }
@@ -414,7 +414,7 @@ function loadAssigneesList() {
 }
 
 function loadProductInfo() {
-    if (bzProduct !== null) {
+    if (bzProduct !== "") {
         httpGet("/rest.cgi/product/" + bzProduct + "?include_fields=has_unconfirmed", function(response) {
             bzProductHasUnconfirmed = response.products[0].has_unconfirmed;
             updateUnconfirmedColumnVisibilty();
@@ -437,7 +437,7 @@ function loadColumns() {
         });
         updateUnconfirmedColumnVisibilty();
 
-        if (bzProduct !== null && bzProductMilestone !== null) {
+        if (bzProduct !== "" && bzProductMilestone !== "") {
             loadBoard();
             showNewBugButton();
         }